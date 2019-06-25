5123a2fb7cd4cdfa695a5f7b1e36b016f98ea7df
nickofc <damiancz112@gmail.com>
2019-06-25 03:07:48 +0200

    added files

---
+++ README.md
@@ -0,0 +1,2 @@
+# magnumploch
+[![Netlify Status](https://api.netlify.com/api/v1/badges/a9d0da2d-6b85-493a-a390-4035f664c207/deploy-status)](https://app.netlify.com/sites/magnumploch/deploys)
---
+++ js/scripts.js
@@ -0,0 +1,21 @@
+ScrollReveal().reveal('#about', {
+    delay: 300
+});
+ScrollReveal().reveal('#offer', {
+    delay: 300
+});
+ScrollReveal().reveal('#contact', {
+    delay: 300
+});
+ScrollReveal().reveal('#intro-info', {
+    delay: 300
+});
+ScrollReveal().reveal('.navbar', {
+    delay: 100
+});
+ScrollReveal().reveal('#intro', {
+    delay: 50
+});
+ScrollReveal().reveal('#intro-button', {
+    delay: 500
+});
